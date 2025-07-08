f29a55a3fe6657f845eede000c8d8e1b1bd2d966
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-08 17:41:26 +0300

    refactor: extend PageHeaderProps from HtmlElementProps to improve DRY (Tue, Jul 08, 2025 05:41 PM)

--- src/components/Templates/PageHeader/interface.ts
+++ src/components/Templates/PageHeader/interface.ts
@@ -1,5 +1,5 @@
-export interface PageHeaderProps {
+import type { HtmlElementProps } from '../../../types/interfaces';
+
+export interface PageHeaderProps extends HtmlElementProps {
   level?: 1 | 2 | 3 | 4 | 5 | 6;
-  className?: string;
-  children?: React.ReactNode;
 }
