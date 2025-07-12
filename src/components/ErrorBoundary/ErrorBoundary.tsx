31be559dc5600c53365887c6680fc16f0b759741
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-12 16:43:03 +0300

    feat: add back button and improve error display styling (Sat, Jul 12, 2025 04:43 PM)

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { Component, type ErrorInfo } from 'react';
 import type { ErrorBoundaryProps, ErrorBoundaryState } from './interface';
 import ErrorMsg from '../Templates/ErrorMsg/ErrorMsg';
+import ButtonTemplate from '../Templates/ButtonTemplate/ButtonTemplate';
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
@@ -20,7 +21,16 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       return (
-        this.props.fallback ?? <ErrorMsg errorMsg="Something went wrong" />
+        <div className="app">
+          {this.props.fallback ?? <ErrorMsg errorMsg="Something went wrong" />}
+
+          <ButtonTemplate
+            className="app__btn-back"
+            onClick={(): void => this.setState({ hasError: false })}
+          >
+            Go back
+          </ButtonTemplate>
+        </div>
       );
     }
 
