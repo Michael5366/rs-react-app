a20cb8938b88028e7136328cba659408d4027b77
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-25 00:31:29 +0300

    refactor: rewrite SearchForm component from class to function (Fri, Jul 25, 2025 12:31 AM)

--- src/components/Search/Form.tsx
+++ src/components/Search/Form.tsx
@@ -1,37 +1,35 @@
-import { Component, type FormEvent } from 'react';
+import { type FormEvent } from 'react';
 import PageHeader from '../Templates/PageHeader/PageHeader';
 import Wrapper from './components/Wrapper/Wrapper';
 import Field from './components/Field/Field';
 import ButtonTemplate from '../Templates/ButtonTemplate/ButtonTemplate';
 import type FormProps from './interface';
 
-class SearchForm extends Component<FormProps> {
-  handleSubmit = (e: FormEvent): void => {
+const SearchForm = ({ onSearch, value, onChange }: FormProps) => {
+  const handleSubmit = (e: FormEvent): void => {
     e.preventDefault();
-    const trimmed = this.props.value.trim();
-    this.props.onSearch(trimmed);
+    const trimmed = value.trim();
+    onSearch(trimmed);
   };
 
-  render() {
-    return (
-      <form
-        className="app__controls"
-        onSubmit={this.handleSubmit}
-        data-testid="search-form"
-      >
-        <PageHeader level={2} className="app__controls-header">
-          Top controls
-        </PageHeader>
+  return (
+    <form
+      className="app__controls"
+      onSubmit={handleSubmit}
+      data-testid="search-form"
+    >
+      <PageHeader level={2} className="app__controls-header">
+        Top controls
+      </PageHeader>
 
-        <Wrapper>
-          <Field value={this.props.value} onChange={this.props.onChange} />
-          <ButtonTemplate className="app__search-btn" type="submit">
-            Let&apos;s go
-          </ButtonTemplate>
-        </Wrapper>
-      </form>
-    );
-  }
-}
+      <Wrapper>
+        <Field value={value} onChange={onChange} />
+        <ButtonTemplate className="app__search-btn" type="submit">
+          Let&apos;s go
+        </ButtonTemplate>
+      </Wrapper>
+    </form>
+  );
+};
 
 export default SearchForm;
