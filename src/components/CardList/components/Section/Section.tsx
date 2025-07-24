c181e7eabb2a989188b29629517ff1380ec8e667
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-25 00:50:15 +0300

    refactor: rewrite Section component from class to function (Fri, Jul 25, 2025 12:50 AM)

--- src/components/CardList/components/Section/Section.tsx
+++ src/components/CardList/components/Section/Section.tsx
@@ -1,24 +1,19 @@
-import { Component } from 'react';
 import PageHeader from '../../../Templates/PageHeader/PageHeader';
 import type { DataProps } from '../../../../types/interfaces';
 import RenderContent from './RenderContent';
 
-class Section extends Component<DataProps> {
-  render() {
-    const { data, loading, error } = this.props;
+const Section = ({ data, loading, error }: DataProps) => {
+  return (
+    <section className="app__results" data-testid="cardListSection">
+      <PageHeader level={2} className="app__results-header">
+        Result
+      </PageHeader>
 
-    return (
-      <section className="app__results" data-testid="cardListSection">
-        <PageHeader level={2} className="app__results-header">
-          Result
-        </PageHeader>
-
-        <section className="app__results-output">
-          <RenderContent data={data} loading={loading} error={error} />
-        </section>
+      <section className="app__results-output">
+        <RenderContent data={data} loading={loading} error={error} />
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default Section;
