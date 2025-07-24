1fc172370228cc5e5b8604dc6c21b3401956b978
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-25 00:00:25 +0300

    refactor: rewrite ThemeToggle component from class to function (Fri, Jul 25, 2025 12:00 AM)

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,23 +1,18 @@
-import { Component } from 'react';
 import type { ThemeToggleProps } from './interface';
 
-class ThemeToggle extends Component<ThemeToggleProps> {
-  render() {
-    const { onToggle, isDark } = this.props;
-
-    return (
+const ThemeToggle = ({ onToggle, isDark }: ThemeToggleProps) => {
+  return (
+    <div
+      className="app__theme-toggle"
+      onClick={onToggle}
+      role="button"
+      aria-label="toggle theme"
+    >
       <div
-        className="app__theme-toggle"
-        onClick={onToggle}
-        role="button"
-        aria-label="toggle theme"
-      >
-        <div
-          className={`app__toggle-thumb ${isDark ? 'app__toggle-thumb--right' : ''}`}
-        ></div>
-      </div>
-    );
-  }
-}
+        className={`app__toggle-thumb ${isDark ? 'app__toggle-thumb--right' : ''}`}
+      ></div>
+    </div>
+  );
+};
 
 export default ThemeToggle;
