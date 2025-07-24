a6737817d0dd16e34037f66ca1e575d733d0092b
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-25 00:26:47 +0300

    refactor: rewrite Field component from class to function (Fri, Jul 25, 2025 12:26 AM)

--- src/components/Search/components/Field/Field.tsx
+++ src/components/Search/components/Field/Field.tsx
@@ -1,19 +1,17 @@
-import { Component, type ChangeEvent } from 'react';
+import { type ChangeEvent } from 'react';
 import type FieldProps from './interface';
 
-class Field extends Component<FieldProps> {
-  render() {
-    return (
-      <input
-        className="app__search-field"
-        placeholder="Search"
-        value={this.props.value}
-        onChange={(e: ChangeEvent<HTMLInputElement>): void => {
-          this.props.onChange?.(e.target.value);
-        }}
-      />
-    );
-  }
-}
+const Field = ({ value, onChange }: FieldProps) => {
+  return (
+    <input
+      className="app__search-field"
+      placeholder="Search"
+      value={value}
+      onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+        onChange?.(e.target.value);
+      }}
+    />
+  );
+};
 
 export default Field;
