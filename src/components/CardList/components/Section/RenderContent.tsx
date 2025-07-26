f6ba69a0535110610d72a1fed144b3f82c317965
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-07-26 23:31:23 +0300

    refactor: rewrite RenderContent component from class to function (Sat, Jul 26, 2025 11:31 PM)

--- src/components/CardList/components/Section/RenderContent.tsx
+++ src/components/CardList/components/Section/RenderContent.tsx
@@ -1,32 +1,27 @@
-import { Component } from 'react';
 import type { DataProps } from '../../../../types/interfaces';
 import ErrorMsg from '../../../Templates/ErrorMsg/ErrorMsg';
 import Spinner from '../../../Spinner/Spinner';
 import Card from '../Card/Card';
 
-class RenderContent extends Component<DataProps> {
-  render() {
-    const { data, loading, error } = this.props;
-
-    return (
-      <>
-        {error && (
-          <ErrorMsg
-            errorMsg={typeof error === 'string' ? error : String(error)}
-          />
-        )}
-        {!error && loading && <Spinner />}
-        {!error &&
-          !loading &&
-          data &&
-          data?.length > 0 &&
-          data.map((item) => <Card key={item.title} item={item} />)}
-        {!error && !loading && (!data || data.length === 0) && (
-          <ErrorMsg errorMsg="Nothing matched your query" />
-        )}
-      </>
-    );
-  }
-}
+const RenderContent = ({ data, loading, error }: DataProps) => {
+  return (
+    <>
+      {error && (
+        <ErrorMsg
+          errorMsg={typeof error === 'string' ? error : String(error)}
+        />
+      )}
+      {!error && loading && <Spinner />}
+      {!error &&
+        !loading &&
+        data &&
+        data?.length > 0 &&
+        data.map((item) => <Card key={item.title} item={item} />)}
+      {!error && !loading && (!data || data.length === 0) && (
+        <ErrorMsg errorMsg="Nothing matched your query" />
+      )}
+    </>
+  );
+};
 
 export default RenderContent;
